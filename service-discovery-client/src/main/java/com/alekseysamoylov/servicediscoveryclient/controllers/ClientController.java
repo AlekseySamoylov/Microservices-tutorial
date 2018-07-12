fcd9f0853f60c9f21d03b8da8fdb809f35f6ada5
Aleksei Samoilov <alekseysamoylov89@gmail.com>
2018-07-12 09:18:53 +0300

    add centralized logging: elk

--- service-discovery-client/src/main/java/com/alekseysamoylov/servicediscoveryclient/controllers/ClientController.java
+++ service-discovery-client/src/main/java/com/alekseysamoylov/servicediscoveryclient/controllers/ClientController.java
@@ -1,5 +1,7 @@
 package com.alekseysamoylov.servicediscoveryclient.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
+
     @Value("${eureka.instance.metadataMap.region}")
     private String region;
 
     @GetMapping("/ping")
     public String ping() {
+        LOGGER.info("Info access to client server with region " + region);
+        LOGGER.debug("Debug access to client server with region " + region);
         return "The region is " + region;
     }
 }
