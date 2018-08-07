c56fb86f9cbb0fdc0123b59ed1d8e34419fec52a
Aleksey Samoylov <alekseysamoylov89@gmail.com>
2018-08-07 10:00:32 +0300

    add profiling to product service

--- product-service/src/main/kotlin/com/alekseysamoylov/productservice/ProductServiceApplication.kt
+++ product-service/src/main/kotlin/com/alekseysamoylov/productservice/ProductServiceApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.feign.EnableFeignClients
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableAspectJAutoProxy
 class ProductServiceApplication
 
 fun main(args: Array<String>) {
--- product-service/src/main/kotlin/com/alekseysamoylov/productservice/controllers/ProductController.kt
+++ product-service/src/main/kotlin/com/alekseysamoylov/productservice/controllers/ProductController.kt
@@ -2,12 +2,14 @@ package com.alekseysamoylov.productservice.controllers
 
 import com.alekseysamoylov.productservice.entity.Product
 import com.alekseysamoylov.productservice.repository.ProductRepository
+import org.javasimon.aop.Monitored
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/product")
 class ProductController {
     private val log = LoggerFactory.getLogger(ProductController::class.java)
 
@@ -16,30 +18,37 @@ class ProductController {
 
     @Value("\${product.prefix}")
     private lateinit var productPrefix: String
-    @GetMapping("/")
-    fun ping(): String {
-        return "Hello kotlin product app"
-    }
 
-    @GetMapping("/product/{productId}")
+    @GetMapping("/v1.1/{productId}")
     fun getProduct(@PathVariable productId: String): Product {
         val product = productRepository.findOne(productId)
         product.name += getStockName()
         return product
     }
 
-    @PostMapping("/product")
+    @PostMapping("/v1.1")
     fun saveProduct(@RequestBody product: Product): Product {
         product.name = "$productPrefix ${product.name}"
         log.info("Product ${product.name} stored")
         return productRepository.save(product)
     }
 
-    @GetMapping("/product")
+    @Monitored
+    @GetMapping("/v1.1")
     fun getProducts(): Collection<Product> {
         val products = productRepository.findAll()
+        Thread.sleep((1000 + (Math.random() * 150)).toLong())
         products.forEach { product ->
             product.name += getStockName()
+        }
+        return products
+    }
+
+    @GetMapping("/v1.0")
+    fun getProductsDeprecated(): Collection<Product> {
+        val products = productRepository.findAll()
+        products.forEach { product ->
+            product.name += "${getStockName()} DEPRECATED API"
         }
         return products
     }
