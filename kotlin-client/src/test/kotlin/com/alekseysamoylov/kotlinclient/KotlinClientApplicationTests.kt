4a80d39527a2fdf24be0c267232b874bed9a2ba0
Aleksey Samoylov <alekseysamoylov89@gmail.com>
2018-08-17 08:07:13 +0300

    add gradle excluding modules configuration

--- kotlin-client/src/test/kotlin/com/alekseysamoylov/kotlinclient/KotlinClientApplicationTests.kt
+++ kotlin-client/src/test/kotlin/com/alekseysamoylov/kotlinclient/KotlinClientApplicationTests.kt
@@ -1,12 +1,9 @@
 package com.alekseysamoylov.kotlinclient
 
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+//@RunWith(SpringRunner::class)
+//@SpringBootTest
 class KotlinClientApplicationTests {
 
     @Test
